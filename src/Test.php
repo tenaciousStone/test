d328046e686bbac36b0b7e0ef8de7897dd76e14e
root <root@localhost.localdomain>
2020-01-10 15:41:12 +0800

    test

---
+++ src/Test.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Notes:
+ * User: zack
+ * version: 2.0
+ * Date: 2020/1/10
+ * Time: 15:19
+ */
+
+namespace Xz;
+
+class Test
+{
+    public function test()
+    {
+        echo 11;
+    }
+}
